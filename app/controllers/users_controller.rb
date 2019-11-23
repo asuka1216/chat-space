229ccef5117f0639bca3377492b5c557dd5840b4
asuka1216 <56636325+asuka1216@users.noreply.github.com>
2019-11-23 15:00:05 +0900

    complited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
-    return nil if params[:keyword] == ""
-    @users = User.where(['name LIkE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
-    respond_to do |format|
-      format.html
-      format.json
+    return nil if params[:keyword] == "" #params[:keyword]に値が入っていればそのまま処理が続き,空だった場合はそこで処理が終了
+    @users = User.where(['name LIkE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10) #whereメソッドを使用し,入力された値を含むかつ,ログインしているユーザーのidは除外する条件で検索処理を行う
+    respond_to do |format| #フォーマットの返し方を条件分岐
+      format.html #フォーマットをhtmlで返す
+      format.json #フォーマットをjsonで返す=>jbuilderファイルが読み込まれる
     end
   end
 

8b32fff8c3878f6dfd4b155d25a59464fe58f397
asuka1216 <56636325+asuka1216@users.noreply.github.com>
2019-11-23 21:40:12 +0900

    complited

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -16,8 +16,8 @@ $(function(){
                   <p class="lower-message__content">
                   ${message.content}
                   </p>
+                  ${image}
                 </div>
-                ${image}
               </div>`
           return html; //htmlをajaxに返す
   }
@@ -45,6 +45,32 @@ $(function(){
     })
       .fail(function() {
         alert("メッセージ送信に失敗しました");
-    })
-  })
+    });
+  });
+
+  $(function(){
+    var reloadMessages = function(){
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+        var last_message_id = $('.message:last').data("message-id");
+        $.ajax({
+          url: "api/messages",
+          type: 'get',
+          dataType: 'json',
+          data: {last_id: last_message_id}
+        })
+        .done(function(messages){
+          var insertHTML = '';
+          messages.forEach(function(message){
+            insertHTML = buildHTML(message);
+            $('.messages').append(insertHTML);
+          })
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 50);
+        })
+        .fail(function(){
+          alert('自動更新に失敗しました');
+        });
+      }
+    };
+    setInterval(reloadMessages, 7000);
+  });
 });
